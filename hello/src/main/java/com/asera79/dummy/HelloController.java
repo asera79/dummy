668a5e0d208087623ddab23cce8ca4717bef084c
Sergey Antonov <sergey.antonov@seriously.com>
2020-02-25 16:37:34 +0200

    Initial commit

---
+++ hello/src/main/java/com/asera79/dummy/HelloController.java
@@ -0,0 +1,13 @@
+package com.asera79.dummy;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller
+public class HelloController {
+    @Get(produces = MediaType.TEXT_PLAIN)
+    public String index() {
+        return "Hello World!";
+    }
+}
